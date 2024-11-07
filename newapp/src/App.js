5cd3403af4d2f125239b9e23ae7986a3ef82e44e
Karishma2422 <karishmapatil74@gmail.com>
2024-11-07 21:14:34 +0530

    day 1

---
+++ newapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+       <div>
+            <h1>React practical session Day-1</h1>
+            <h1>KARISHMA</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
