61b90d19be65af43b17744bf3156d12a674fa5d4
Karishma2422 <karishmapatil74@gmail.com>
2024-11-26 12:17:37 +0530

    day 3

--- newapp/src/components/Home.jsx
+++ newapp/src/components/Home.jsx
@@ -1,10 +1,22 @@
+import { useNavigate } from "react-router-dom"
+
 function Home (){
+    const router = useNavigate()
+    function redirectToLoginPage(){
+        router("/Login");
+    }
+    function redirectToRegisterPage(){
+        router("/Register");
+    }
+    function redirectToUseStatePage(){
+        router("/UseState");
+    }
     return(
         <div>
             <h1>Welcome to Home page </h1>
-            <button>GO TO LOGIN </button>
-            <button>GO TO REGISTER </button>
-            <button>GO TO USESTATE </button>
+            <button onClick={redirectToLoginPage}>GO TO LOGIN </button>
+            <button onClick={redirectToRegisterPage}>GO TO REGISTER </button>
+            <button onClick={redirectToUseStatePage}>GO TO USESTATE </button>
         </div>
     )
 }
--- newapp/src/components/day3/UseStates.jsx
+++ newapp/src/components/day3/UseStates.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+
+
 function UseState(){
+    const [counter , setcounter] =useState(1);
+    console.log(counter,"Counter");
+    function increment(){
+        setcounter(counter + 1 );
+    }
+    function decrement(){
+        
+        setcounter(counter - 1);
+        
+    }
+    function reset() {
+        setcounter(1);
+    }
     return(
-                    <h1>UseState </h1>
+                <div>
+                    {/* // <h1>This is UseState function</h1> */}
+                    <h1>Counter :{counter}</h1>
+                    <button onClick={increment}>+</button>
+                    <button onClick={reset}>Reset</button>
+                    <button onClick={decrement}>-</button>
+                    </div>
     )
 }
 
